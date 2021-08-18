277b5778083914fb38c124d3b14b17a51ca50f45
FuzzballZ <cronjevosloo.jc@gmail.com>
2021-08-18 09:56:07 +0200

    Change Date

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace CTU_Vaccine_Development
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Type your details and press enter
+            Console.WriteLine("Enter your ID");
+            Console.WriteLine("Enter your Surname");
+            Console.WriteLine("Enter your First Name");
+            Console.WriteLine("Enter Gender");
+            Console.WriteLine("Enter your Date Registration");
+            Console.WriteLine("Enter the province");
+
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            int ID = Convert.ToInt32(Console.ReadLine());
+            string SurN = Console.ReadLine();
+            string FirstName = Console.ReadLine();
+            string Gend = Console.ReadLine();
+            int DOB = Convert.ToInt32(Console.ReadLine());
+            string Prov = Console.ReadLine();
+
+            // Print the value of the variable User Input, which will display the input value
+            Console.WriteLine("Your ID is: " + ID);
+            Console.WriteLine("Surname is: " + SurN);
+            Console.WriteLine("First Name is: " + FirstName);
+            Console.WriteLine("Your Gender is: " + Gend);
+            Console.WriteLine("The Date of Registration is on: " + DOB);
+            Console.WriteLine("They province that you're live is: " + Prov);
         }
     }
 }
