ee8935196f844d199ae3ce6e7b245cb221da51cb
FuzzballZ <cronjevosloo.jc@gmail.com>
2021-08-18 09:52:57 +0200

    Vaccine Project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CTU_Vaccine_Development
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
